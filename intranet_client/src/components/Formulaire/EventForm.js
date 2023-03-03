5b762e2d58c35594f9ef43d563673974695821e3
Derreumaux <valentinderreumaux03@gmail.com>
2023-03-03 13:53:04 +0100

    update Calendar Form

--- intranet_client/src/components/Formulaire/EventForm.js
+++ intranet_client/src/components/Formulaire/EventForm.js
@@ -1,115 +1,111 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 import axios from 'axios';
-import "../../styles/Formulaire/EventForm.css";
+import '../../styles/Formulaire/EventForm.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-const EventForm = ({ onSubmit }) => {
-  const [title, setTitle] = useState('');
-  const [start, setStart] = useState('');
-  const [end, setEnd] = useState('');
-  const [location, setLocation] = useState('');
-  const [description, setDescription] = useState('');
-  const [calendar, setCalendar] = useState('');
+const EventForm = () => {
+    const [title, setTitle] = useState('');
+    const [debut, setDebut] = useState('');
+    const [fin, setFin] = useState('');
+    const [location, setLocation] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [formCompleted, setFormCompleted] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-    const event = {
-      title,
-      debut: start,
-      fin: end,
-      location,
-      description,
-      category: calendar,
+        axios
+            .post('http://localhost:8002/api/event/', {
+                title,
+                Debut: debut,
+                Fin: fin,
+                location,
+                description,
+                category,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
-    axios.post('http://localhost:8002/api/event/', event)
-      .then(() => {
-        setTitle('');
-        setStart('');
-        setEnd('');
-        setLocation('');
-        setDescription('');
-        setCalendar('');
-        onSubmit();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    const handleInputChange = (e) => {
+        setTitle(e.target.title);
+        setDebut(e.target.debut);
+        setFin(e.target.fin);
+        setLocation(e.target.location);
+        setDescription(e.target.description);
+        setCategory(e.target.category);
 
-  return (
-    <form onSubmit={handleSubmit} className="event-form">
-      <label htmlFor="title" className="event-form__label">Title</label>
-      <input
-        id="title"
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="event-form__input"
-        required
-      />
+        const completed = e.target.title && e.target.debut && e.target.fin && e.target.location && e.target.description && e.target.category;
+        setFormCompleted(completed);
+    };
 
-      <label htmlFor="start" className="event-form__label">Start</label>
-      <input
-        id="start"
-        type="datetime-local"
-        value={start}
-        onChange={(e) => setStart(e.target.value)}
-        className="event-form__input"
-        required
-      />
+    return (
+        <form className="event-form" onSubmit={handleSubmit}>
+            <div className="form-row">
+                <div className="form-col">
+                    <label className="form-label">
+                        Titre :
+                        <input className="form-input" type="text" value={title}
+                               onChange={(e) => setTitle(e.target.value)}
+                               title="Saisissez le titre de l'événement ici."/>
+                    </label>
+                    <br/>
+                    <label className="form-label">
+                        Lieu :
+                        <input className="form-input" type="text" value={location}
+                               onChange={(e) => setLocation(e.target.value)}
+                               title="Saisissez lieu et/ou la salle de l'événement"/>
+                    </label>
+                </div>
+                <div className="form-col">
+                    <label className="form-label">
+                        Début :
+                        <input className="form-input" type="datetime-local" value={debut}
+                               onChange={(e) => setDebut(e.target.value)}
+                               title="Saisissez la date et l'heure de début de l'événement"/>
+                    </label>
+                    <br/>
+                    <label className="form-label">
+                        Fin :
+                        <input className="form-input" type="datetime-local" value={fin}
+                               onChange={(e) => setFin(e.target.value)}
+                               title="Saisissez la date et l'heure de fin de l'événement"/>
+                    </label>
+                </div>
+            </div>
+            <label className="form-label">
+                Description :
+                <ReactQuill className="form-input" value={description} onChange={(value) => setDescription(value)}/>
+            </label>
+            <br/>
+            <div className="form-row">
+                <div className="form-col">
+                    <label className="form-label">
+                        Calendrier :
+                        <select className="form-select" value={category} onChange={(e) => setCategory(e.target.value)}
+                                title="Sélectionnez le calendrier de l'événement">
+                            <option value="">Sélectionnez un calendrier</option>
+                            <option value="1">CBP</option>
+                            <option value="2">CFS</option>
+                            <option value="3">SCH</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="form-col">
+                    <button className="form-button" type="submit"
+                            disabled={!title || !debut || !fin || !location || !description || !description}>Enregistrer
+                    </button>
+                </div>
+            </div>
+        </form>
 
-      <label htmlFor="end" className="event-form__label">End</label>
-      <input
-        id="end"
-        type="datetime-local"
-        value={end}
-        onChange={(e) => setEnd(e.target.value)}
-        className="event-form__input"
-        required
-      />
-
-      <label htmlFor="location" className="event-form__label">Location</label>
-      <input
-        id="location"
-        type="text"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-        className="event-form__input"
-        required
-      />
-
-      <label htmlFor="description" className="event-form__label">Description</label>
-      <textarea
-        id="description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        className="event-form__textarea"
-        required
-      />
-
-      <label htmlFor="calendar" className="event-form__label">Calendar</label>
-      <select
-        id="calendar"
-        value={calendar}
-        onChange={(e) => setCalendar(e.target.value)}
-        className="event-form__select"
-        required
-      >
-        <option value="">Select a calendar</option>
-        <option value="1">Calendar 1</option>
-        <option value="2">Calendar 2</option>
-        <option value="3">Calendar 3</option>
-      </select>
-
-      <button type="submit" className="event-form__submit-btn">Submit</button>
-    </form>
-  );
-};
-
-EventForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+    );
 };
 
 export default EventForm;
